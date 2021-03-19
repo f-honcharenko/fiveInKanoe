84420a248f0f7e54c4976c6d2820ae64aee85481
Vitalii Povkh <fehold2000@gmail.com>
2021-03-19 18:34:26 +0200

    new maps added

---
+++ core/assets/Maps/MyTileMap.tsx
@@ -0,0 +1,24 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="MyTileMap" tilewidth="64" tileheight="64" tilecount="6400" columns="80">
+ <image source="MyTileMap.png" width="5120" height="5120"/>
+ <terraintypes>
+  <terrain name="Shdow grass" tile="81"/>
+ </terraintypes>
+ <tile id="0" terrain=",,,0"/>
+ <tile id="1" terrain=",,0,0"/>
+ <tile id="2" terrain=",,0,"/>
+ <tile id="3" terrain="0,0,0,"/>
+ <tile id="4" terrain="0,0,,"/>
+ <tile id="5" terrain="0,0,,0"/>
+ <tile id="80" terrain=",0,,0"/>
+ <tile id="81" terrain="0,0,0,0"/>
+ <tile id="82" terrain="0,,0,"/>
+ <tile id="83" terrain="0,,0,"/>
+ <tile id="85" terrain=",0,,0"/>
+ <tile id="160" terrain=",0,,"/>
+ <tile id="161" terrain="0,0,,"/>
+ <tile id="162" terrain="0,,,"/>
+ <tile id="163" terrain="0,,0,0"/>
+ <tile id="164" terrain=",,0,0"/>
+ <tile id="165" terrain=",0,0,0"/>
+</tileset>
