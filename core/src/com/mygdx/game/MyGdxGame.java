a18843f6d8feed2f8d0edb814672be4e0c4ef834
Filipp Honcharenko <akstoplay@gmail.com>
2021-03-27 12:48:16 +0200

    add Scenes and Screens

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -1,74 +1,73 @@
 package com.mygdx.game;
 
-import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Input.Keys;
-import com.badlogic.gdx.graphics.GL20;
-// import com.badlogic.gdx.graphics.Camera;
-import com.badlogic.gdx.graphics.OrthographicCamera;
-// import com.badlogic.gdx.graphics.Texture;
-// import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.maps.tiled.TiledMap;
-import com.badlogic.gdx.maps.tiled.TmxMapLoader;
-import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
-// import com.badlogic.gdx.utils.ScreenUtils;
-import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.mygdx.game.Screens.PlayScreen;
 
-public class MyGdxGame extends ApplicationAdapter {
+public class MyGdxGame extends Game {
 
-	TiledMap tiledMap;
-	OrthogonalTiledMapRenderer ortMapRender;
-	OrthographicCamera camera;
-	FitViewport viewport;
-	int[] map;
+	public SpriteBatch batch;
+	public static final int V_WIDTH = 400;
+	public static final int V_HEIGHT = 208;
+	// TiledMap tiledMap;
+	// OrthogonalTiledMapRenderer ortMapRender;
+	// OrthographicCamera camera;
+	// FitViewport viewport;
+	// int[] map;
 
 	@Override
 	public void create() {
-		camera = new OrthographicCamera();
-		tiledMap = new TmxMapLoader().load("Maps/Exit.tmx");
-		ortMapRender = new OrthogonalTiledMapRenderer(tiledMap);
-		camera.setToOrtho(false, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-		// camera.translate(256, 0);
-		camera.update();
-		viewport = new FitViewport(Gdx.graphics.getWidth(), Gdx.graphics.getHeight(), camera);
+		batch = new SpriteBatch();
+		setScreen(new PlayScreen(this));
+		// camera = new OrthographicCamera();
+		// tiledMap = new TmxMapLoader().load("Maps/Exit.tmx");
+		// ortMapRender = new OrthogonalTiledMapRenderer(tiledMap);
+		// camera.setToOrtho(false, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+		// // camera.translate(256, 0);
+		// camera.update();
+		// viewport = new FitViewport(Gdx.graphics.getWidth(), Gdx.graphics.getHeight(),
+		// camera);
 	}
 
 	@Override
 	public void render() {
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT); // фикс мерцания экрана при изменении
-																				// размеров окна
-		if (Gdx.input.isKeyPressed(Keys.UP)) {
-			camera.position.y++;
-		}
-		if (Gdx.input.isKeyPressed(Keys.DOWN)) {
-			camera.position.y--;
-		}
-		if (Gdx.input.isKeyPressed(Keys.LEFT)) {
-			camera.position.x--;
-		}
-		if (Gdx.input.isKeyPressed(Keys.RIGHT)) {
-			camera.position.x++;
-		}
+		super.render();
 
-		// SWITCH MAP [start.tmx]
-		if (Gdx.input.isKeyPressed(Keys.Q)) {
-			tiledMap = new TmxMapLoader().load("Maps/start.tmx");
-			ortMapRender = new OrthogonalTiledMapRenderer(tiledMap);
-		}
-		// SWITCH MAP [room.tmx]
-		if (Gdx.input.isKeyPressed(Keys.W)) {
-			tiledMap = new TmxMapLoader().load("Maps/room.tmx");
-			ortMapRender = new OrthogonalTiledMapRenderer(tiledMap);
-		}
-		// SWITCH MAP [exit.tmx]
-		if (Gdx.input.isKeyPressed(Keys.E)) {
-			tiledMap = new TmxMapLoader().load("Maps/exit.tmx");
-			ortMapRender = new OrthogonalTiledMapRenderer(tiledMap);
-		}
-		camera.update();
-		viewport.update(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-		ortMapRender.setView((OrthographicCamera) viewport.getCamera());
-		ortMapRender.render();
+		// Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT); // фикс
+		// мерцания экрана при изменении
+		// // размеров окна
+		// if (Gdx.input.isKeyPressed(Keys.UP)) {
+		// camera.position.y++;
+		// }
+		// if (Gdx.input.isKeyPressed(Keys.DOWN)) {
+		// camera.position.y--;
+		// }
+		// if (Gdx.input.isKeyPressed(Keys.LEFT)) {
+		// camera.position.x--;
+		// }
+		// if (Gdx.input.isKeyPressed(Keys.RIGHT)) {
+		// camera.position.x++;
+		// }
+
+		// // SWITCH MAP [start.tmx]
+		// if (Gdx.input.isKeyPressed(Keys.Q)) {
+		// tiledMap = new TmxMapLoader().load("Maps/start.tmx");
+		// ortMapRender = new OrthogonalTiledMapRenderer(tiledMap);
+		// }
+		// // SWITCH MAP [room.tmx]
+		// if (Gdx.input.isKeyPressed(Keys.W)) {
+		// tiledMap = new TmxMapLoader().load("Maps/room.tmx");
+		// ortMapRender = new OrthogonalTiledMapRenderer(tiledMap);
+		// }
+		// // SWITCH MAP [exit.tmx]
+		// if (Gdx.input.isKeyPressed(Keys.E)) {
+		// tiledMap = new TmxMapLoader().load("Maps/exit.tmx");
+		// ortMapRender = new OrthogonalTiledMapRenderer(tiledMap);
+		// }
+		// camera.update();
+		// viewport.update(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+		// ortMapRender.setView((OrthographicCamera) viewport.getCamera());
+		// ortMapRender.render();
 	}
 
 	@Override
---
+++ core/src/com/mygdx/game/Screens/PlayScreen.java
@@ -0,0 +1,74 @@
+package com.mygdx.game.Screens;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
+import com.mygdx.game.MyGdxGame;
+import com.mygdx.game.Scenes.Hud;
+
+public class PlayScreen implements Screen {
+    private MyGdxGame game;
+    private OrthographicCamera gameCam;
+    private Viewport gamePort;
+    private Hud hud;
+
+    public PlayScreen(MyGdxGame game) {
+        this.game = game;
+        gameCam = new OrthographicCamera();
+        // gamePort = new StretchViewport(800, 480, gameCam);
+        // gamePort = new ScreenViewport(gameCam);
+        gamePort = new FitViewport(MyGdxGame.V_WIDTH, MyGdxGame.V_HEIGHT, gameCam);
+        hud = new Hud(game.batch);
+    }
+
+    @Override
+    public void show() {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void render(float delta) {
+        Gdx.gl.glClearColor(1, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        game.batch.setProjectionMatrix(hud.stage.getCamera().combined);
+        hud.stage.draw();
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        gamePort.update(width, height);
+
+    }
+
+    @Override
+    public void pause() {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void resume() {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void hide() {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void dispose() {
+        // TODO Auto-generated method stub
+
+    }
+
+}
