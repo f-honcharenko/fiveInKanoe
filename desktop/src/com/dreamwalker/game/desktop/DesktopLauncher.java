e5c7924d48d55ed008cb12e48535ffb6a07ec851
Filipp Honcharenko <akstoplay@gmail.com>
2021-03-30 12:45:28 +0300

    edit screen extension

--- desktop/src/com/dreamwalker/game/desktop/DesktopLauncher.java
+++ desktop/src/com/dreamwalker/game/desktop/DesktopLauncher.java
@@ -5,8 +5,10 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.dreamwalker.game.DreamWalker;
 
 public class DesktopLauncher {
-	public static void main (String[] arg) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new DreamWalker(), config);
-	}
+  public static void main(String[] arg) {
+    LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+    new LwjglApplication(new DreamWalker(), config);
+    config.width = 1920;
+    config.height = 1080;
+  }
 }
