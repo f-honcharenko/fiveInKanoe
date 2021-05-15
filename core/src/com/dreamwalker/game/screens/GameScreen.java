0f7cc22a9122ef67632a34945eac5c8eae80eff6
Filipp Honcharenko <akstoplay@gmail.com>
2021-05-15 15:42:17 +0300

    edit mainGameMenu

--- core/src/com/dreamwalker/game/screens/GameScreen.java
+++ core/src/com/dreamwalker/game/screens/GameScreen.java
@@ -5,6 +5,8 @@ import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 import com.badlogic.gdx.math.Vector2;
