bb0b3ebfdf5a94815d0a8f9049e547d0e6090787
Filipp Honcharenko <akstoplay@gmail.com>
2021-06-04 09:49:57 +0300

    clean code

--- core/src/com/dreamwalker/game/screens/GameScreen.java
+++ core/src/com/dreamwalker/game/screens/GameScreen.java
@@ -30,38 +30,26 @@ public class GameScreen implements Screen {
 
     private Location location;
 
-    /**
-     * Конструктор экрана игры
-     *
-     * @param game - экземпляр основного класса игры
-     */
     public GameScreen(DreamWalker game) {
         MapChanger.setLevelGraph(new LevelGraph("Maps/", 6));
         MapChanger.getLevelGraph().print();
         MapChanger.setCurrentVertex(MapChanger.getLevelGraph().getStart());
         this.game = game;
 
-        // Загрузка карты и создание коллизий
-        this.location = MapChanger.getCurrentVertex().getLocation(); // !!!
+        this.location = MapChanger.getCurrentVertex().getLocation();
 
         this.player = new Player(location.getWorld(), this.location.getSpawnPoint());
 
         this.hud = new Hud(this.game.getBatch(), this.player);
 
         this.camera = new OrthographicCamera();
-        // Прогрузка карты
         this.ortMapRender = new OrthogonalTiledMapRenderer(null, 1 / DreamWalker.PPM);
-        // Установка Ортоганальная проекция камеры, центрированная по вьюпорту
-        // (первый параметр отвечает за направление оси у)
+
         this.camera.setToOrtho(false, Gdx.graphics.getWidth() / DreamWalker.PPM,
                 Gdx.graphics.getHeight() / DreamWalker.PPM);
 
-        // Задаём масштабируемый вьюпорт, с сохранением соотношения сторон
         this.viewport = new ScreenViewport(this.camera);
         this.camera.zoom = 0.6f;
-
-        // this.dstr = new Destroyer(this.location.getWorld());
-        // this.items = new ItemInWorld[10];
 
     }
 
@@ -70,17 +58,11 @@ public class GameScreen implements Screen {
 
     }
 
-    /**
-     * Метод, отвечающий за обновление позиций камеры, установки для "прогрузчика"
-     * гранц рендера, обновление вьюпорта
-     *
-     * @param deltaTime - время тика
-     */
     public void update(float deltaTime) {
         if (!this.player.isAlive()) {
             ScreenSwitcher.toMainMenu();
         }
-        // Реализация "времени" в игровом мире
+
         this.location.getWorld().step(1 / 60f, 6, 2);
         Vector3 mousePosition = this.camera.unproject(new Vector3(Gdx.input.getX(), Gdx.input.getY(), 0));
         this.player.update(deltaTime, new Vector2(mousePosition.x, mousePosition.y));// !
@@ -101,32 +83,6 @@ public class GameScreen implements Screen {
         if (Gdx.input.isKeyJustPressed(Input.Keys.I)) {
             ScreenSwitcher.toInventory(player.getInventory());
         }
-        // // this.pause();
-        // PotionHP tempItem = new PotionHP(1);
-        // ItemInWorld tempItmInWorld = new ItemInWorld(this.player.getX(),
-        // this.player.getY(), tempItem,
-        // this.player.getWorld());
-        // // this.items[this.items.length] = tempItmInWorld;
-        // // this.items.add(tempItmInWorld);
-        // // this.items.add(tempItmInWorld);
-        // AllItemsInWorld.addItem(tempItmInWorld);
-        // // this.player.getInventory().putItem(new PotionHP(1));
-        // // this.screenSwitcher.ToGameMenu();
-        // }
-        // if (Gdx.input.isKeyJustPressed(Input.Keys.P)) {
-        // PotionMP tempItem = new PotionMP(1);
-        // ItemInWorld tempItmInWorld = new ItemInWorld(this.player.getX(),
-        // this.player.getY(), tempItem,
-        // this.player.getWorld());
-        // // this.items[this.items.length] = tempItmInWorld;
-        // // this.items.add(tempItmInWorld);
-        // AllItemsInWorld.addItem(tempItmInWorld);
-
-        // // this.player.getInventory().putItem(new PotionMP(1));
-
-        // }
-
-        // dstr.destroyBody2D();
     }
 
     @Override
@@ -136,52 +92,29 @@ public class GameScreen implements Screen {
 
         update(delta);
 
-        // Цвет окна и фикс мерцания экрана при изменении
         Gdx.gl.glClearColor(0, 0, 0, 1f);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
         this.ortMapRender.setMap(this.location.getMap());
         this.ortMapRender.render();
 
-        // Получение привычных координат мыши (начало в левом НИЖНЕМ углу)
-        // Координаты мыши в пространстве игрового мира
-
-        // Камера должна следовать за игроком
         this.camera.position.x = this.player.getX();
         this.camera.position.y = this.player.getY();
 
-        // рендер игрока
         this.game.getBatch().setProjectionMatrix(this.camera.combined);
         this.game.getBatch().begin();
-        this.player.render(this.game.getBatch());
-        // рендер вещей
-        // if (AllItemsInWorld.getItemList() != null) {
-        // for (int i = 0; i < AllItemsInWorld.getItemList().size(); i++) {
-        // if (AllItemsInWorld.getItemList().get(i) != null) {
-        // ((ItemInWorld)
-        // AllItemsInWorld.getItemList().get(i)).draw(this.game.getBatch());
-        // }
-        // }
-        // }
 
-        // рендер npc
+        this.player.render(this.game.getBatch());
         this.location.enemiesRender(this.game.getBatch());
-        // render
         this.location.itemsRender(this.game.getBatch());
 
         this.game.getBatch().end();
 
-        // Рендер верхнего слоя
-        // MapLayer foregroundLayer = location.getMap().getLayers().size();
-        // пусть foreground -- всгда нулевой слой
         int[] foregroundLayer2 = { location.getMap().getLayers().size() - 1 };
         this.ortMapRender.render(foregroundLayer2);
 
-        // Рендер элементов отладки
-        // this.debugRenderer.render(this.location.getWorld(), this.camera.combined);
-
         this.game.getBatch().setProjectionMatrix(this.hud.getStage().getCamera().combined);
-        // Отрисовка интерфейса
+
         this.hud.getStage().draw();
     }
 
@@ -195,14 +128,12 @@ public class GameScreen implements Screen {
 
     @Override
     public void resize(int width, int height) {
-        // Обновление вьюпорта при изменении размеров окна
         this.viewport.update(width, height);
     }
 
     @Override
     public void pause() {
         System.out.println("pause");
-        // this.game.setScreen(new Sc);
     }
 
     @Override
@@ -219,21 +150,12 @@ public class GameScreen implements Screen {
         return this.player;
     }
 
-    /**
-     * Очистка памяти от ресурсов, используемых игрок
-     */
     @Override
     public void dispose() {
         MapChanger.getLevelGraph().dispose();
         this.player.dispose();
         this.ortMapRender.dispose();
     }
-
-    /*
-     * dispose() работает, как free в С и delete в С++ Диспоузить надо все ресурсы,
-     * которые используются в игре
-     *
-     */
 
     public Location getLocation() {
         return location;
